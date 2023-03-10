76e422ff29c421f9d34635702aec92b7ee5a2b63
saurabh pal <palsaurabh64@gmail.com>
2023-03-10 18:18:38 +0530

    file of webpage

---
+++ script.js
@@ -0,0 +1,115 @@
+const gameInfo = document.querySelector(".game-info");
+const boxes = document.querySelectorAll(".box");
+const newGameBtn = document.querySelector(".btn");
+
+//
+let currentPlayer;
+let gameGrid;
+
+
+const winningPos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
+// lets creat a function to initailize a game
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["", "", "", "", "", "", "", "", ""];
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        box.classList = `box box${index + 1}`;
+    })
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+
+}
+initGame();
+
+function swapTurn() {
+    if (currentPlayer === "X") {
+        currentPlayer = "O";
+    }
+    else {
+        currentPlayer = "X";
+    }
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+
+}
+
+function gameOverCheck() {
+    let ans = "";
+    winningPos.forEach((position) => {
+        //checking at postion 0,1,2 have same element and non empty
+        if ((gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "")
+            && (gameGrid[position[0]] === gameGrid[position[1]]) && gameGrid[position[1]] === gameGrid[position[2]]) {
+            if (gameGrid[position[0]] === "X")
+                ans = "X";
+            else
+                ans = "O";
+
+            //disable pointer event
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            })
+
+            // adding wins class at winning position
+            boxes[position[0]].classList.add("win");
+            boxes[position[1]].classList.add("win");
+            boxes[position[2]].classList.add("win");
+
+        }
+    })
+
+    // we know we have winner
+    if (ans !== "") {
+        gameInfo.innerText = `Winner Player - ${ans}`;
+        newGameBtn.classList.add("active");
+        return;
+    }
+
+    // we know no winner found lets check game is tie
+
+    let fillCount = 0;
+    gameGrid.forEach((box) => {
+        if (box !== "")
+            fillCount++;
+    })
+
+    // boxes is filled game is tie
+    if (fillCount === 9) {
+        gameInfo.innerText = `Game Tied `;
+        newGameBtn.classList.add("active");
+    }
+
+
+}
+function handleClick(index) {
+    if (gameGrid[index] === "") {
+        boxes[index].innerText = currentPlayer;
+        gameGrid[index] = currentPlayer;
+        boxes[index].style.pointerEvents = "none";
+        // turn chance
+        swapTurn();
+        //koi jeet to nahi gaaya
+        gameOverCheck();
+    }
+}
+
+boxes.forEach((box, index) => {
+    box.addEventListener('click', () => {
+        handleClick(index);
+    })
+})
+
+newGameBtn.addEventListener('click', initGame);
+
+
+
